a9bbd5998493b2898e338a7ddb0adf9bf1e86d5c
Vitor <vitorpgmdr@gmail.com>
2021-08-18 21:04:16 -0300

    adiciona mais um exemplo e comenta linhas

--- aula05_grupos_e_retrovisores.py
+++ aula05_grupos_e_retrovisores.py
@@ -11,37 +11,38 @@
 import re
 from pprint import pprint
 
-print('\nSeleciona tag e conteudo:')
 texto = '''
 <p>Frase 1</p> <p>Frase 2</p> <p>Frase 3</p> <div>Frase 4</div>
 '''
 
-pprint(re.findall(r'<([pdiv]{1,3})>(.*?)<\/\1>', texto))
+# print('\nSeleciona tag e conteudo:')
+# pprint(re.findall(r'<([pdiv]{1,3})>(.*?)<\/\1>', texto))
 
-pprint(re.findall(r'(<([pdiv]{1,3})>(.*?)<\/\2>)', texto))
+# pprint(re.findall(r'(<([pdiv]{1,3})>(.*?)<\/\2>)', texto))
 
-# grupo nomeado
-tags = re.findall(r'(<(?P<tag>[pdiv]{1,3})>(.*?)<\/(?P=tag)>)', texto)
-print('nomeado')
-pprint(tags)
+# # grupo nomeado
+# tags = re.findall(r'(<(?P<tag>[pdiv]{1,3})>(.*?)<\/(?P=tag)>)', texto)
+# print('nomeado')
+# pprint(tags)
 
-# (?: ) o ?: no inicio do grupo faz com que ele não seja salvo
-pprint(re.findall(r'<([pdiv]{1,3})>(?:.*?)<\/\1>', texto))
+# # (?: ) o ?: no inicio do grupo faz com que ele não seja salvo
+# pprint(re.findall(r'<([pdiv]{1,3})>(?:.*?)<\/\1>', texto))
 
-pprint(re.findall(r'<(.+?)>(.*?)<\/\1>', texto))
-pprint(re.findall(r'<(.+?)>(.*?)<\/\1>', texto))
+# pprint(re.findall(r'<(.+?)>(.*?)<\/\1>', texto))
+# pprint(re.findall(r'<(.+?)>(.*?)<\/\1>', texto))
 
-pprint(re.findall(r'(<p>.*?</p>)', texto))
+# pprint(re.findall(r'(<p>.*?</p>)', texto))
 
 
 # Remontar a tag html
-print('\nRemonta a tag HTML:')
-print(re.sub(r'(<(.+?)>)(.*?)(<\/\2>)', r'\1Meu texto\3\4', texto))
+# pprint('\nRemonta a tag HTML:')
+# pprint(re.sub(r'(<(.+?)>)(.*?)(<\/\2>)', r'\1Meu texto\3\4', texto))
+pprint(re.findall(r'(?:<(.+?)>).*?(<\/\1>)', texto))
 
 
-print('\nSeleciona cpf:')
-cpf = '012.345.678-90'
-# seleciona o cpf
-print(re.findall(r'[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}', cpf))
-# seleciona o cpf
-print(re.findall(r'((?:[0-9]{3}\.){2}[0-9]{3}-[0-9]{2})', cpf))
+# print('\nSeleciona cpf:')
+# cpf = '012.345.678-90'
+# # seleciona o cpf
+# print(re.findall(r'[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}', cpf))
+# # seleciona o cpf
+# print(re.findall(r'((?:[0-9]{3}\.){2}[0-9]{3}-[0-9]{2})', cpf))
